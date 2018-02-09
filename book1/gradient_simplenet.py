6e9ec60f190c66ac3419e1125af26ba4e33adb98
sukhyun <sukhyun5@gmail.com>
2018-02-09 17:46:04 +0900

    [20180209-02] gradient simplenet 	modified:   gradient_simplenet.py

--- book1/gradient_simplenet.py
+++ book1/gradient_simplenet.py
@@ -5,16 +5,38 @@ import numpy as np
 def sigmoid(x):
     return 1/ (1+np.exp(-x))
 
+"""
 def softmax(a):
     c = np.max(a)
     exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
+"""
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
 
+"""
 def cross_entropy_error(y, t):
     delta = 1e-7
     return -np.sum(t*np.log(y+delta))
+"""
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 def _numerical_gradient_1d(f, x):
     h = 1e-4
@@ -23,7 +45,7 @@ def _numerical_gradient_1d(f, x):
     for idx in range(x.size):
         tmp_val = x[idx]
 
-        x[idx] = tmp_val + h
+        x[idx] = float(tmp_val) + h
         fxh1 = f(x)
         x[idx] = tmp_val - h
         fxh2 = f(x)
