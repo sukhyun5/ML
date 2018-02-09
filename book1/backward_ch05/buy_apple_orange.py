2910b4f0a4bfc48d52b4dc0ed002c8275483340f
sukhyun <sukhyun5@gmail.com>
2018-02-09 17:42:17 +0900

    [20180209] chaper 05 end. 	new file:   buy_apple.py 	new file:   buy_apple_orange.py 	new file:   gradient_check.py 	new file:   layer_naive.py 	new file:   layers.py 	new file:   softmax_with_loss.py 	new file:   train_neuralnet.py 	new file:   two_layer_net.py 	modified:   ../gradient_simplenet.py

---
+++ book1/backward_ch05/buy_apple_orange.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+from layer_naive import *
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+# layer
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+# backward : price -> all_price -> apple_price, orange_price
+dprice = 1 # start this value
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)  
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+
+print("price:", int(price))
+print("dall_price:", dall_price)
+print("dtax:", dtax)
+print("dapple_price:", dapple_price)
+print("dorange_price:", dorange_price)
+print("dapple:", dapple)
+print("dapple_num:", int(dapple_num))
+print("dorange:", dorange)
+print("dorange_price:", int(dorange_num))
+
+
+
---
+++ book1/backward_ch05/layers.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+import numpy as np
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout*self.out(1.0-self.out)
+        return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+        
+        
