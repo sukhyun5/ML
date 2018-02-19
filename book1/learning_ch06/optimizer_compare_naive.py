edb6bdb9fe1bb47133e1aacdcbd9f67777281778
sukhyun <sukhyun5@gmail.com>
2018-02-19 20:19:41 +0900

    ch06

---
+++ book1/learning_ch06/optimizer_compare_naive.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from optimizer import *
+#from optimizer_orig import *
+
+def f(x, y):
+    return x**2 / 20.0 + y**2
+
+def df(x, y):
+    return x / 10.0, 2.0*y
+
+init_pos = (-7.0, 2.0)
+params = {}
+params['x'], params['y'] = init_pos[0], init_pos[1]
+grads = {}
+grads['x'], grads['y'] = 0, 0
+
+optimizers = OrderedDict()
+optimizers["SGD"] = SGD(lr=0.95)
+optimizers["Momentum"] = Momentum(lr=0.1)
+optimizers["AdaGrad"] = AdaGrad(lr=1.5)
+optimizers["Adam"] = Adam(lr=0.3)
+
+idx = 1
+
+for key in optimizers:
+    optimizer = optimizers[key]
+    x_history = []
+    y_history = []
+    params['x'], params['y'] = init_pos[0], init_pos[1]
+
+    for i in range(30):
+        print ('key[%s]'%key, params['x'], params['y'])
+        x_history.append(params['x'])
+        y_history.append(params['y'])
+
+        grads['x'], grads['y'] = df(params['x'], params['y'])
+        optimizer.update(params, grads)
+
+
